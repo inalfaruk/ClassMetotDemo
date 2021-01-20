58a02f561dcaa2b87a817c0c8d5e422df5a68fac
farukinal16 <farukinal16@gmail.com>
2021-01-20 20:31:38 +0300

    son

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace ClassMetotDemo
+{
+    public class MusteriManager
+    {
+
+        public static void Listele(Musteri m)
+        {
+            Console.WriteLine("Müşteri Listesi");
+
+
+        }
+
+        public static void Ekle (Musteri m)
+        {
+            
+            Console.WriteLine(m.Ad + " " + m.Soyad + "Müşteri Eklendi");
+
+        }
+
+        public static void Guncelle (Musteri m)
+        {
+
+            Console.WriteLine("Müşteri Bilgileri Güncellendi");
+        }
+
+
+
+        public static void Sil(Musteri m)
+        {
+
+            Console.WriteLine(m.Ad+" "+m.Soyad+"İsimli Müşteri Silindi");
+        }
+    }
+}
